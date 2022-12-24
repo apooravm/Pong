7b911a847d4125085216483d39678dfb1df1b016
Apoorav <medal.apoorav@gmail.com>
2022-12-24 19:38:01 +0530

    working build

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ let p1;
 let allPaddles = [];
 let allBalls = [];
 
+let score_LEFT = 0;
+let score_RIGHT = 0;
+
 function setup() {
   createCanvas(800, 400);
   allPaddles.push(new Paddle(0.05*width, 0.5*height));
@@ -14,16 +17,24 @@ function draw() {
   background(51);
   drawMiddleLine();
 
+  displaySCORES();
+
   for (let paddle of allPaddles) {
     paddle.show();
     paddle.update(mouseY);
   }
 
   for (let ball of allBalls) {
-    ball.collision(X_val=true, paddleYN=false);
+    ball.collision(false, true, allPaddles);
     ball.update();
     ball.show();
   }
+}
+
+function displaySCORES() {
+  textSize(16);
+  text(`${score_LEFT}`, 0.25*width, 0.1*height);
+  text(`${score_RIGHT}`, 0.75*width, 0.1*height);
 }
 
 function drawMiddleLine(val=10, skip=20) {
@@ -58,7 +69,8 @@ class Ball
     // range b/w 0-1
     // scalable
     this.accerelation = createVector(this.scale_VAL*random(0.4, 1), this.scale_VAL*random(0.4, 1));
-    this.velocity = createVector(this.accerelation.x, this.accerelation.y);
+    // this.velocity = createVector(this.accerelation.x, this.accerelation.y);
+    this.velocity = createVector(0, 0);
 
     this.isStuckToWallY = false;
     this.isStuckToWallX = false;
@@ -70,14 +82,10 @@ class Ball
   update()
   {
     this.prev_X = this.x;
-    // this.velocity.add(this.accerelation);
-    // this.velocity += this.accerelation;
-    // this.accerelation.mult(0);
+    this.velocity.add(this.accerelation);
+    this.accerelation.mult(0);
 
-    // text([this.velocity.x, this.velocity.y], mouseX, mouseY);
-    text(random(0.4, 1)*-1, mouseX, mouseY);
-
-    // this.velocity.limit(this.MAX_velocity);
+    this.velocity.limit(this.MAX_velocity);
     this.x += this.velocity.x;
     this.y += this.velocity.y;
   }
@@ -89,53 +97,91 @@ class Ball
     circle(this.x, this.y, this.radius*2);
   }
 
-  initAcceleration(rightSide=false)
+  initAcceleration(rightSide)
   {
     // Returns a y value, either positive or negative in a certain range
-    let get_YVAL = () => {
-      if (random() < 0.5) {
-        // Negative Y
-        return -1 * random(0.4, 1);
-      } else {
-        // Positive Y
-        return random(0.4, 1);
-      }
+    let get_YVAL;
+    if (random() < 0.5) {
+       // Negative Y
+       get_YVAL =  -1 * random(0.4, 1);
+    } else 
+    {
+      // Positive Y
+      get_YVAL = random(0.4, 1);
     }
+
     //Right side collision
     if (rightSide) {
-      this.x *= this.NEG_SCALING_val;
-      this.velocity = createVector(this.scale_VAL*random(0.4, 1)*(-1), this.scale_VAL * get_YVAL());
+      this.velocity.x *= this.NEG_SCALING_val;
+      // this.velocity.mult(-1);
+      this.velocity = createVector(this.scale_VAL*random(0.4, 1.1)*(-1), this.scale_VAL * get_YVAL);
     } else {
       //Left Side collision
-      this.x *= this.NEG_SCALING_val;
-      this.velocity = createVector(this.scale_VAL*random(0.4, 1), this.scale_VAL * get_YVAL());
+      this.velocity.x *= this.NEG_SCALING_val;
+      // this.velocity.mult(-1);
+      this.velocity = createVector(this.scale_VAL*random(0.4, 1.1), this.scale_VAL * get_YVAL);
     }
+  }
+
+  randomizeVelocity()
+  {
+    let get_YVAL;
+    if (random() < 0.5) {
+       // Negative Y
+       get_YVAL =  -1 * random(0.4, 1);
+    } else 
+    {
+      // Positive Y
+      get_YVAL = random(0.4, 1);
+    }
+
+    let get_XVAL;
+    if (random() < 0.5) {
+       // Negative Y
+       get_XVAL =  -1 * random(0.4, 1);
+    } else 
+    {
+      // Positive Y
+      get_XVAL = random(0.4, 1);
+    }
+    this.velocity = createVector(this.scale_VAL * get_XVAL, this.scale_VAL * get_YVAL);
   }
 
   collision(X_val=true, paddleYN=false, paddles=false)
   {
     if (X_val) {
-      //Left Side Collision
-      if (this.x + this.radius + this.MIN_wallDist > width && !this.isStuckToWallX) {
-        this.initAcceleration(false);
-        this.isStuckToWallX = true;
-      } 
       //Right Side Collision
-      else if (this.x - this.radius - this.MIN_wallDist < 0 && !this.isStuckToWallX) {
+      if (this.x + this.radius + this.MIN_wallDist > width && !this.isStuckToWallX) {
         this.initAcceleration(true);
+        this.isStuckToWallX = true;
+      }
+      //Left Side Collision
+      else if (this.x - this.radius - this.MIN_wallDist < 0 && !this.isStuckToWallX) {
+        this.initAcceleration(false);
         this.isStuckToWallX = true;
       } else {
         this.isStuckToWallX = false;
       }
+    } else {
+      if (this.x > width + 40) {
+        score_LEFT += 1;
+        this.x = width/2;
+        this.y = height/2;
+        this.randomizeVelocity();
+
+      } else if (this.x < 0 - 40) {
+        score_RIGHT += 1;
+        this.x = width/2;
+        this.y = height/2;
+        this.randomizeVelocity();
+      }
     }
 
-    if (this.y + this.radius + this.MIN_wallDist > height || 
-        this.y - this.radius - this.MIN_wallDist < 0 ) {
-          if (!this.isStuckToWallY) {
-            // Flips the x from positive => negative or vice versa
-            this.velocity.y *= this.NEG_SCALING_val;
-            this.isStuckToWallY = true;
-          }
+    if ((this.y + this.radius + this.MIN_wallDist > height || 
+        this.y - this.radius - this.MIN_wallDist < 0) && !this.isStuckToWallY) {
+          // Flips the y from positive => negative or vice versa
+          this.velocity.y *= this.NEG_SCALING_val;
+          this.isStuckToWallY = true;
     } else {
       this.isStuckToWallY = false;
     }
@@ -150,15 +196,15 @@ class Ball
             !this.isStuckToPaddle) {
           if (this.x - this.prev_X >= 0) {
             // Moving to the right/ hitting from the Right Side
-            this.initAcceleration(rightSide=true);
+            this.initAcceleration(true);
           } else {
             // hitting from the Left Side
-            this.initAcceleration(rightSide=false);
+            this.initAcceleration(false);
           }
           this.isStuckToPaddle = true;
         } 
         else {
-          this.isStuckToPaddle;
+          this.isStuckToPaddle = false;
         }
       }
     }
@@ -171,7 +217,7 @@ class Paddle
   {
     this.x = x;
     this.y = y;
-    this.width = 20;
+    this.width = 40;
     this.height = 100;
     this.colour = [random(255), random(255), random(255)];
   }
